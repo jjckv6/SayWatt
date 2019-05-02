073994f91880677ac52ebc07acf10c87044ea4cb
Josiah Cal <josiahc12345@gmail.com>
2019-05-02 13:38:55 -0500

    more git fixes

--- UI/ViewController.swift
+++ UI/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class HomeViewController: UIViewController{
+class ViewController: UIViewController{
     
     @IBOutlet weak var settingsButton: UIButton!
     override func viewDidLoad() {
